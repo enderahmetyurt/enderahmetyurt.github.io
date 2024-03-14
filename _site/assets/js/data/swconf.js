0f16cd8e5e6584e535191378d0cda98279f24129
Ender Ahmet Yurt <enderyurt@gmail.com>
2024-03-14 21:14:17 +0300

    Use https

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710439824',resources: [
+    cacheName: 'chirpy-1710440047',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
