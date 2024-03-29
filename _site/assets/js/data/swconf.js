f5b7302396d7f67a7341dae3294a67e9b0afcdc7
Ender Ahmet Yurt <enderyurt@gmail.com>
2024-03-29 17:54:40 +0300

    New post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711029777',resources: [
+    cacheName: 'chirpy-1711713503',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
