2691a93ae5555ca636b155b9ecf5f058c90c42e8
Ender Ahmet Yurt <enderyurt@gmail.com>
2024-04-04 17:54:34 +0300

    Make enable to add comments

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711713503',resources: [
+    cacheName: 'chirpy-1712242285',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
