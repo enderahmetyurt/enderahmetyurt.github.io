5a55e7d92815cd689ee9d2d914d813b7ad9c8573
Ender Ahmet Yurt <enderyurt@gmail.com>
2024-05-24 08:47:16 +0300

    Add talk

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714593604',resources: [
+    cacheName: 'chirpy-1716467521',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
