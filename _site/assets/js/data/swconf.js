0592b07a7d7f33f5756f1a66b9b2b0c9627e8bd4
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-05 17:21:15 +0300

    Update cache name and timestamps; modify post links in various files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738765251',resources: [
+    cacheName: 'chirpy-1738765265',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
