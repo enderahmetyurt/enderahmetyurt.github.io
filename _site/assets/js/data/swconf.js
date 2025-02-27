4d0e6d5a4e18478de09a52ff63af6b14b346b840
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-27 20:58:53 +0300

    Add Twitter

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740579152',resources: [
+    cacheName: 'chirpy-1740679110',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
