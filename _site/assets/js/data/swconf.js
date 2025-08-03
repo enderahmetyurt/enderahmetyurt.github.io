158149dcb4ac5897b5a1a848dc9898d52e429c93
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-08-03 13:43:00 +0300

    Remove post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752559259',resources: [
+    cacheName: 'chirpy-1754217763',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
