a0e1c17dd577050baffb3a0abd8e4ea5bdb46db3
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-08-08 09:01:12 +0300

    Update profile pic resource

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754217763',resources: [
+    cacheName: 'chirpy-1754632811',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
